f2eb458efb7f1f0605e09ef24ef6c3d9d85c34e2
danceslikesnake <dennis@getwala.com>
2018-02-14 04:53:26 -0600

    tweaks

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 
 // styles ans scripts
 import '../../utils/styles/boilerplate.css';
+import '../../utils/styles/slick.css';
+import '../../utils/styles/slick-theme.css';
 
 // components
 import Hero from '../Hero/Hero';
